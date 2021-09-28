f6a3bcb881ab791b279e20a77f86d3c23670a28f
xiemingzhang.ok@163.com <xiemingzhang.ok@163.com>
2021-09-28 20:18:45 +0800

    发布包括liveGame

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const prodUrlRoot = "https://61cr.cn/ai/game/"
 let AIGameUrlsData = {
 	data: []
 }
-// let LiveGameUrlsData = {
-// 	data: []
-// }
+let LiveGameUrlsData = {
+	data: []
+}
 
 let targetFile = process.argv[2]
 let parentLocation = path.resolve(targetFile, "../../")
@@ -49,15 +49,15 @@ let bundles = jsonObj.bundleVers
 //                 ]
  
 let AiGameKeys = []
+let LiveGameKeys = []
 let bundleVers = {}
-// let LiveGameKeys = []
 Object.keys(bundles).forEach((item)=>{
     if(item.startsWith("AiGame")){
         AiGameKeys.push(item)
     }
-    // if(item.startsWith("LiveGame")){
-    //     LiveGameKeys.push(item)
-    // }
+    if(item.startsWith("LiveGame")){
+        LiveGameKeys.push(item)
+    }
     if(item == "internal"
     || item == "WH_frame"
     || item == "resources"
@@ -70,9 +70,29 @@ Object.keys(bundles).forEach((item)=>{
 AiGameKeys = AiGameKeys.sort((item1, item2)=>{
     return item1.split("_")[0].split("AiGame")[1] - item2.split("_")[0].split("AiGame")[1] 
 })
-// LiveGameKeys = LiveGameKeys.sort((item1, item2)=>{
-//     return item1.split("_")[0].split("LiveGame")[1] - item2.split("_")[0].split("LiveGame")[1] 
-// })
+LiveGameKeys = LiveGameKeys.sort((item1, item2)=>{
+    return item1.split("_")[0].split("LiveGame")[1] - item2.split("_")[0].split("LiveGame")[1] 
+})
+
+function createGameProj(name) {
+    let itemLocation = path.resolve(gameLocation + "/" + name)
+    let itemFile = path.resolve(itemLocation, "index.html")
+    // console.log(itemFile)
+    fs.mkdirSync(itemLocation)
+
+    let _Str = IndexStr.replace(/\.\//g, '../')
+    _Str = _Str.replace("XXXXXX", name)
+
+    _Str = _Str.replace("FBXID", bundles["FBX"])
+    _Str = _Str.replace("LoadingID", bundles["Loading"])
+    _Str = _Str.replace("TeachLayerID", bundles["TeachLayer"])
+    _Str = _Str.replace("TrainLayerID", bundles["TrainLayer"])
+    _Str = _Str.replace("ScoreNumID", bundles["ScoreNum"])
+    _Str = _Str.replace("AddScoreID", bundles["AddScore"])
+    _Str = _Str.replace("GAMEID", bundles[name])
+    
+    fs.writeFileSync(itemFile, _Str)
+}
 
 AiGameKeys.forEach((item) => {
     let _obj = {}
@@ -84,34 +104,21 @@ AiGameKeys.forEach((item) => {
     _obj["gameDuration"] = 60
     AIGameUrlsData.data.push(_obj)
 
-    let itemLocation = path.resolve(gameLocation + "/" + item)
-    let itemFile = path.resolve(itemLocation, "index.html")
-    // console.log(itemFile)
-    fs.mkdirSync(itemLocation)
-
-    let _Str = IndexStr.replace(/\.\//g, '../')
-    _Str = _Str.replace("XXXXXX", item)
-
-    _Str = _Str.replace("FBXID", bundles["FBX"])
-    _Str = _Str.replace("loadingID", bundles["loading"])
-    _Str = _Str.replace("TeachLayerID", bundles["TeachLayer"])
-    _Str = _Str.replace("TrainLayerID", bundles["TrainLayer"])
-    _Str = _Str.replace("ScoreNumID", bundles["ScoreNum"])
-    _Str = _Str.replace("AddScoreID", bundles["AddScore"])
-    _Str = _Str.replace("GAMEID", bundles[item])
-    
-    fs.writeFileSync(itemFile, _Str)
+    createGameProj(item)
 })
 
-// LiveGameKeys.map((item) => {
-//     let _obj = {}
-//     _obj["step"] = item.split("_")[0].split("AiGame")[1]
-//     _obj["fragmentName"] = ""
-//     _obj["gameUrl"] = prodUrlRoot + "?gameID=" + bundles[item] + "&liveGameName=" + item
-//     _obj["gameCount"] = 40
-//     _obj["gameDuration"] = 60
-//     LiveGameUrlsData.data.push(_obj)
-// })
+LiveGameKeys.map((item) => {
+    let _obj = {}
+    _obj["step"] = item.split("_")[0].split("LiveGame")[1]
+    _obj["fragmentName"] = ""
+    // _obj["gameUrl"] = prodUrlRoot + "?gameID=" + bundles[item] + "&liveGameName=" + item
+    _obj["gameUrl"] = prodUrlRoot + item
+    _obj["gameCount"] = 40
+    _obj["gameDuration"] = 60
+    LiveGameUrlsData.data.push(_obj)
+
+    createGameProj(item)
+})
 
 console.log("settings.bundleVers")
 console.log(`"bundleVers": {
@@ -121,7 +128,10 @@ console.log(`"bundleVers": {
 }
 `)
 
+console.log("LiveGameUrlsData")
+console.log(JSON.stringify(LiveGameUrlsData, null, 4))
+
 console.log("AIGameUrlsData")
-console.log(AIGameUrlsData)
+console.log(JSON.stringify(AIGameUrlsData, null, 4))
 
 
