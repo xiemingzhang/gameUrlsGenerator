73dd0958a26be34265a23f077ca2cea4b2ae0a08
xiemingzhang.ok@163.com <xiemingzhang.ok@163.com>
2021-09-28 11:30:55 +0800

    no message

--- index1.js
+++ index1.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const fs = require('fs-extra')
+const fs = require('fs')
 const path = require('path')
 
 const prodUrlRoot = "https://61cr.cn/ai/game/"
@@ -12,27 +12,7 @@ let AIGameUrlsData = {
 // 	data: []
 // }
 
-let targetFile = process.argv[2]
-let parentLocation = path.resolve(targetFile, "../../")
-let gameLocation = path.resolve(parentLocation, "./game")
-
-let indexFile = path.resolve(parentLocation, "./index.html")
-if(fs.existsSync(gameLocation)){
-    fs.emptyDirSync(gameLocation)
-}else{
-    fs.mkdirSync(gameLocation)
-}
-
-// console.log(targetFile)
-// console.log(parentLocation)
-// console.log(gameLocation)
-// console.log(indexFile)
-
-let IndexStr = fs.readFileSync(indexFile).toString()
-// console.log(IndexStr)
-// return
-
-let str = fs.readFileSync(targetFile).toString().split("=")[1];
+let str = fs.readFileSync(process.argv[2]).toString().split("=")[1];
 eval("var jsonObj=" + str)
 
 let bundles = jsonObj.bundleVers
@@ -74,33 +54,14 @@ AiGameKeys = AiGameKeys.sort((item1, item2)=>{
 //     return item1.split("_")[0].split("LiveGame")[1] - item2.split("_")[0].split("LiveGame")[1] 
 // })
 
-AiGameKeys.forEach((item) => {
+AiGameKeys.map((item) => {
     let _obj = {}
     _obj["step"] = item.split("_")[0].split("AiGame")[1]
     _obj["fragmentName"] = ""
-    // _obj["gameUrl"] = prodUrlRoot + "?gameID=" + bundles[item] + "&aiGameName=" + item
-    _obj["gameUrl"] = prodUrlRoot + item
+    _obj["gameUrl"] = prodUrlRoot + "?gameID=" + bundles[item] + "&aiGameName=" + item
     _obj["gameCount"] = 40
     _obj["gameDuration"] = 60
     AIGameUrlsData.data.push(_obj)
-
-    let itemLocation = path.resolve(gameLocation + "/" + item)
-    let itemFile = path.resolve(itemLocation, "index.html")
-    // console.log(itemFile)
-    fs.mkdirSync(itemLocation)
-
-    let _Str = IndexStr.replace(/\.\//g, '../')
-    _Str = _Str.replace("XXXXXX", item)
-
-    _Str = _Str.replace("FBXID", bundles["FBX"])
-    _Str = _Str.replace("loadingID", bundles["loading"])
-    _Str = _Str.replace("TeachLayerID", bundles["TeachLayer"])
-    _Str = _Str.replace("TrainLayerID", bundles["TrainLayer"])
-    _Str = _Str.replace("ScoreNumID", bundles["ScoreNum"])
-    _Str = _Str.replace("AddScoreID", bundles["AddScore"])
-    _Str = _Str.replace("GAMEID", bundles[item])
-    
-    fs.writeFileSync(itemFile, _Str)
 })
 
 // LiveGameKeys.map((item) => {
@@ -119,6 +80,37 @@ console.log(`"bundleVers": {
     "WH_frame": "${bundles["WH_frame"]}",
     "main": "${bundles["main"]}"
 }
+`)
+
+console.log("mainModify")
+console.log(`
+// if(!standBy){
+    var gameName = aiGameName
+    if(liveGameName){
+      bundles.push("FBX")
+      bundles.push("Loading")
+      bundles.push("TeachLayer")
+      bundles.push("TrainLayer")
+      settings.bundleVers["FBX"] = "${bundles["FBX"]}"
+      settings.bundleVers["Loading"] = "${bundles["Loading"]}"
+      settings.bundleVers["TeachLayer"] = "${bundles["TeachLayer"]}"
+      settings.bundleVers["TrainLayer"] = "${bundles["TrainLayer"]}"
+      gameName = liveGameName
+    }
+    
+    gameName && bundles.push(gameName)
+    gameID && (settings.bundleVers[gameName] = gameID)
+  // }else{
+  //   bundles.push("StandPositionAdjustment")
+  //   settings.bundleVers["StandPositionAdjustment"] = "${bundles["StandPositionAdjustment"]}"
+  // }
+  
+  if (dev) {
+    bundles.push("ScoreNum")
+    bundles.push("AddScore")
+    settings.bundleVers["ScoreNum"] = "${bundles["ScoreNum"]}"
+    settings.bundleVers["AddScore"] = "${bundles["AddScore"]}"
+  }
 `)
 
 console.log("AIGameUrlsData")
